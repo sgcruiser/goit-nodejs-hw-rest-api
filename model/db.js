715debeefd7e77b27a2e8dbb892086174718b5a7
Sergey Glazyrin <glazyrinsb@gmail.com>
2021-07-10 23:34:07 +0300

    Update db.js

--- model/db.js
+++ model/db.js
@@ -14,20 +14,32 @@ const db = async () => {
 }
 
 mongoose.connection.on('connected', (_) => {
-  console.info('\x1b[92m%s\x1b[0m', 'Database connection successful')
+  console.info(
+    '\x1b[92m%s\x1b[0m',
+    'Database connection successful'
+  )
 })
 
 mongoose.connection.on('error', (error) => {
-  console.info('\x1b[91m%s\x1b[0m', `Database connection error: ${error.message}`)
+  console.info(
+    '\x1b[91m%s\x1b[0m',
+    `Database connection error: ${error.message}`
+  )
 })
 
 mongoose.connection.on('disconnected', (_) => {
-  console.info('\x1b[94m%s\x1b[0m', 'Database disconnected')
+  console.info(
+    '\x1b[94m%s\x1b[0m',
+    'Database disconnected'
+  )
 })
 
 process.on('SIGINT', async () => {
   mongoose.connection.close(() => {
-    console.info('\x1b[35m%s\x1b[0m', 'Connection to DB closed and app termination')
+    console.info(
+      '\x1b[35m%s\x1b[0m',
+      'Connection to DB closed and app termination'
+    )
     process.exit(1)
   })
 })
